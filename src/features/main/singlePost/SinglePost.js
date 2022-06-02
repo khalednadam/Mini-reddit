e2ea0bd007620fe2a037bae8774c5944dcf28335
Khaled Nadam <khalednadam@gmail.com>
2022-06-02 20:12:19 +0300

    adding header to single post

---
+++ src/features/main/singlePost/SinglePost.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Header } from '../../header/Header';
+import './singlePost.css';
+export const SinglePost = () =>{
+    return(
+        <div className='single-post'>
+            <Header />
+        </div>
+    );
+}
